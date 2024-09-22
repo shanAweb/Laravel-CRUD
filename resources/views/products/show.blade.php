812e96c0830427c1bbdc6fbace54704d75267396
Shan Asif <shanasif2002@gmail.com>
2024-09-22 11:51:03 +0500

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-header">Product Details</div>
 
                     <div class="card-body">
-                        <p>Shan Asif </p>
+                        <p>Shan Asif Created this:::::</p>
                         <p><strong>Name:</strong> {{ $product->name }}</p>
                         <p><strong>Description:</strong> {{ $product->description }}</p>
                         <p><strong>Price:</strong> {{ $product->price }}</p>
